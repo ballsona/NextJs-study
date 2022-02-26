3405d419c161951a426f724b4513c880327c9e4f
ballsona0925 <ballsona0925@naver.com>
2022-02-26 23:15:01 +0900

    lecture 1.6 ~ 1.7

--- nextjs-intro/components/Navbar.tsx
+++ nextjs-intro/components/Navbar.tsx
@@ -1,18 +1,43 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   const router = useRouter();
   return (
     <nav>
       <Link href="/">
-        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/" ? "active" : ""
+          }`}
+        >
+          Home
+        </a>
       </Link>
       <Link href="/about">
-        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+        <a
+          className={`${styles.link} ${
+            router.pathname === "/about" ? "active" : ""
+          }`}
+        >
           About
         </a>
       </Link>
+
+      <style jsx>
+        {`
+          nav {
+            background-color: #d8d8d8;
+            display: flex;
+            justify-content: space-between;
+            padding: 20px;
+          }
+          .active {
+            color: yellow;
+          }
+        `}
+      </style>
     </nav>
   );
 }
---
+++ nextjs-intro/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { AppProps } from "next/dist/shared/lib/router/router";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <div>Hello</div>
+    </>
+  );
+}
